1685f5975ba6724866ea6f6f1ad53ecd7bd67536
ff245185 <ff245185@gmail.com>
2022-08-22 21:37:20 +0600

    fast visit

---
+++ main.js
@@ -0,0 +1,15 @@
+<div id="id = " head1" class="bg - black row row - cols - 3 mx - auto my - 5 ">
+    < div class="row" >
+    <div class="col">
+
+        <img class="img-fluid w-50" src="images/ronaldo1.jpg" alt="">
+    </div>
+    <div class="col">
+        <h1><i class="text-primary">Select you best players</i></h1>
+        <p class="text-white">EA SPORTS™ FIFA 23 brings The World's Game to the pitch, with next-gen
+            HyperMotion2
+            Technology, women's club football, two FIFA World Cup™ tournaments,</p>
+    </div>
+
+</div >
+</div >
