777e78b4cea4e068043e844b722510f40dc5127d
ff245185 <ff245185@gmail.com>
2022-08-23 22:14:13 +0600

    third

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 const shopArray = [];
 
+function displayPt(cardProduct1) {
+    let tootalPrice = 0;
+    console.log(cardProduct1)
+    const tList = document.getElementById('viwe-1');
+    tList.innerHTML = '';
+    for (let i = 0; i < cardProduct1.length; i++) {
+        // console.log(shopArray[i].messiProduct, shopArray[i].playerProductPrice);
+        const name = shopArray[i].messiProduct;
+        const price = shopArray[i].playerProductPrice;
+
+        tootalPrice = tootalPrice + price;
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <th>${i}</th>
+        <td>${name}</td>
+        <td>${price}</td>
+        `;
+
+        tList.appendChild(tr);
+    }
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+         <th></th>
+        <td>Tootal-price:</td>
+        <td>${tootalPrice}</td>
+        
+    `
+    tList.appendChild(tr)
+}
+
+
 
 function adToCard(element) {
     // console.log(element.parentNode.children)
@@ -19,5 +50,6 @@ function adToCard(element) {
 
     // console.log(shopArray);
     // console.log(shopArray.length);
-    document.getElementById('total-man').innerText = shopArray.length
+    document.getElementById('total-man').innerText = shopArray.length;
+    displayPt(shopArray)
 }
