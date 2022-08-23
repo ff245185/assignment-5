745fe5fd3001b0d28b5779d7283e5c83c49820ae
ff245185 <ff245185@gmail.com>
2022-08-23 20:59:48 +0600

    next push

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-<div id="id = " head1" class="bg - black row row - cols - 3 mx - auto my - 5 ">
-    < div class="row" >
-    <div class="col">
+const shopArray = [];
 
-        <img class="img-fluid w-50" src="images/ronaldo1.jpg" alt="">
-    </div>
-    <div class="col">
-        <h1><i class="text-primary">Select you best players</i></h1>
-        <p class="text-white">EA SPORTS™ FIFA 23 brings The World's Game to the pitch, with next-gen
-            HyperMotion2
-            Technology, women's club football, two FIFA World Cup™ tournaments,</p>
-    </div>
 
-</div >
-</div >
+function adToCard(element) {
+    // console.log(element.parentNode.children)
+
+
+    const messiProduct = element.parentNode.children[0].innerText;
+    // console.log(messiProduct);
+    const playerProductPrice = element.parentNode.children[1].children[0].innerText;
+    // console.log(playerProductPrice);
+
+    const ProductObject = {
+        messiProduct: messiProduct,
+        playerProductPrice: parseFloat(playerProductPrice)
+
+    }
+    shopArray.push(ProductObject)
+
+    // console.log(shopArray);
+    // console.log(shopArray.length);
+    document.getElementById('total-man').innerText = shopArray.length
+}
